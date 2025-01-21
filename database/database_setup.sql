897247459e3f7f5edcf9b44accddc1f16e9c253a
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2025-01-21 12:16:40 +0000

    add sql query to create database and tables

---
+++ database/database_setup.sql
@@ -0,0 +1,134 @@
+-- Create the database
+IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = 'Adventour')
+BEGIN
+    CREATE DATABASE Adventour;
+END;
+GO
+
+-- Use the database
+USE Adventour;
+GO
+
+-- Create the tables
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Country')
+BEGIN
+    CREATE TABLE Country (
+        id_country INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'City')
+BEGIN
+    CREATE TABLE City (
+        id_city INT PRIMARY KEY,
+        id_country INT NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        FOREIGN KEY (id_country) REFERENCES Country(id_country)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Person')
+BEGIN
+    CREATE TABLE Person (
+        id_user INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        phone_number VARCHAR(20),
+        verified BIT NOT NULL,
+        profile_picture BIT NULL,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Attraction')
+BEGIN
+    CREATE TABLE Attraction (
+        id_attraction INT PRIMARY KEY,
+        id_city INT NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        average_rating INT NULL,
+        description VARCHAR(MAX),
+        address VARCHAR(255) NULL,
+        FOREIGN KEY (id_city) REFERENCES City(id_city)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Attraction_Info')
+BEGIN
+    CREATE TABLE Attraction_Info (
+        id_attraction_info INT PRIMARY KEY,
+        id_attraction INT NOT NULL,
+        title VARCHAR(255),
+        description VARCHAR(MAX),
+        duration_seconds INT NULL,
+        FOREIGN KEY (id_attraction) REFERENCES Attraction(id_attraction)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Rating')
+BEGIN
+    CREATE TABLE Rating (
+        id_rating INT PRIMARY KEY,
+        value INT NOT NULL
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Review')
+BEGIN
+    CREATE TABLE Review (
+        id_review INT PRIMARY KEY,
+        id_rating INT NOT NULL,
+        id_attraction INT NOT NULL,
+        id_user INT NOT NULL,
+        comment VARCHAR(MAX) NULL,
+        FOREIGN KEY (id_rating) REFERENCES Rating(id_rating),
+        FOREIGN KEY (id_attraction) REFERENCES Attraction(id_attraction),
+        FOREIGN KEY (id_user) REFERENCES Person(id_user)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Favorites')
+BEGIN
+    CREATE TABLE Favorites (
+        id_favorite INT PRIMARY KEY,
+        id_attraction INT NOT NULL,
+        id_user INT NOT NULL,
+        FOREIGN KEY (id_attraction) REFERENCES Attraction(id_attraction),
+        FOREIGN KEY (id_user) REFERENCES Person(id_user)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Itinerary')
+BEGIN
+    CREATE TABLE Itinerary (
+        id_itinerary INT PRIMARY KEY,
+        id_user INT NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        created_at DATETIME NOT NULL,
+        FOREIGN KEY (id_user) REFERENCES Person(id_user)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Day')
+BEGIN
+    CREATE TABLE Day (
+        id_day INT PRIMARY KEY,
+        id_itinerary INT NOT NULL,
+        day_number INT NOT NULL,
+        FOREIGN KEY (id_itinerary) REFERENCES Itinerary(id_itinerary)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Timeslot')
+BEGIN
+    CREATE TABLE Timeslot (
+        id_time_slot INT PRIMARY KEY,
+        id_attraction INT NOT NULL,
+        id_day INT NOT NULL,
+        start_time DATETIME NOT NULL,
+        end_time DATETIME NOT NULL,
+        FOREIGN KEY (id_attraction) REFERENCES Attraction(id_attraction),
+        FOREIGN KEY (id_day) REFERENCES Day(id_day)
+    );
+END;
