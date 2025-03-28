18cf645bc76cc776c9193db3b621a2d304e48d6e
Gonçalo Neves <guga.santos.neves@gmail.com>
2025-03-28 02:06:49 +0000

    City Structure
    Needed to an Attraction validation

---
+++ api/Responses/City/City.cs
@@ -0,0 +1,10 @@
+﻿namespace Adventour.Api.Responses.City
+{
+
+    public class City
+    {
+        public int Id { get; set; }
+        public int IdCountry { get; set; }
+        public string Name { get; set; }
+    }
+}
